39fc6a0c87811336601206c09266be24788bb20f
pimlu <pimlu@users.noreply.github.com>
2016-05-19 19:58:41 -0400

    create compiler stub, add else/special characters to parser
    still a lot of work to do in comp.js.  next up: evalIntExpr, then
    filling in ops in build

---
+++ src/comp.js
@@ -0,0 +1,111 @@
+'use strict';
+
+let _ = require('lodash');
+
+const config = {
+  main: 'main',
+  maxDepth: 100
+};
+
+function comp(tree, o) {
+  //make options default to config when things aren't set
+  //not allowed to use Proxy yet since it breaks debuggers
+  /*opt = new Proxy(o || {}, {
+    get: (target, key) => key in target ?
+        target[key] :
+        config[key],
+  });*/
+  let opt = Object.assign({}, config, o);
+  let main = opt.main;
+  //make a parse tree of modules
+  function modRec(rules, parent) {
+    let dict = {};
+    for(let mod of rules) {
+      if(mod.t !== 'module') continue;
+      dict[mod.name] = {
+        mod: mod,
+        submods: modRec(mod.rules, dict),
+        parent: parent
+      };
+    }
+    return dict;
+  }
+  //used for looking up modules
+  function lookup(key, dict) {
+    if(!dict) return null;
+    else if(key in dict.submods) return dict.submods[key];
+    else return lookup(key, dict.parent);
+  }
+  //builds a particular module (concretely - it's given parameters for templates)
+  function doComp(main, dict, tree, params, past) {
+    let ret = {};//TODO
+    
+    let entry = lookup(main, dict);
+    let mod = entry.mod;
+    //console.log(entry);
+    //basic error checking
+    let modName = main+(params.length?`<${params.join(',')}>`:'');
+    if(mod === null) throw new Error(`module ${modName} not found`);
+    let templ = mod.template;
+    if(params.length !== templ.length) throw new Error(`module ${main} passed ${params.length} params, expected ${templ.length}`);
+    if(past.length >= opt.maxDepth) throw new Error(`module ${modName} reached max recursion depth (${config()})`)
+    for(let p of past) {
+      if(main === p.main && _.isEqual(params, p.params)) throw new Error(`cycle detected for module ${modName}`);
+    }
+    //end of error checking
+    
+    //evaluating numeric values - used for templates of modules
+    let ctx = _.zipObject(templ, params);
+    let iOpsChar = '+-*/'.split('');
+    let iOps = _.zipObject(iOpsChar, iOpsChar.map(
+      c => eval(`(function(a,b) { return a ${c} b | 0; })`
+    )));
+    function evalIntExpr(expr) {
+      if(Array.isArray(expr)) {
+        return iOps[expr[0]](
+          evalIntExpr(expr[1]),
+          evalIntExpr(expr[2]));
+      }
+      //FIXME
+      return expr;
+    }
+    //builds a particular set of rules within a particular module context
+    function build(rules, cond) {
+      
+      let exportVar = (name, rule) => {
+        if(cond) throw new Error(`${name} can't be inside if statement`);
+      };
+      //rules for each type of statement
+      let ops = {
+        var: rule => {
+          
+        },
+        module: rule => {
+          if(cond) throw new Error(`module can't be inside if statement`);
+        },
+        'if': rule => {
+          
+        },
+        join: rule => {
+          
+        },
+        modref: rule => {
+        },
+        input: exportVar.bind(null, 'input'),
+        output: exportVar.bind(null, 'output')
+      };
+      //scan through each rule
+      for(let rule of rules) {
+        console.log(rule);
+        let fn = ops[rule.t];
+        fn(rule);
+      }
+    }
+    build(mod.rules);
+  }
+  let dict = modRec(tree, null);
+  //uses submods at the top to cooperate with the lookup function
+  return doComp(main, {submods:dict}, tree, [], []);
+}
+
+module.exports = comp;
--- src/index.js
+++ src/index.js
@@ -5,21 +5,19 @@ let fs = Promise.promisifyAll(require('fs')),
   path = require('path'),
   PEG = require('pegjs');
 
+let comp = require('./comp.js');
+
 //TODO cache parser
 let lang = fs.readFileSync(path.resolve(__dirname, 'lang.peg'), 'utf8');
 let parser = PEG.buildParser(lang);
 
-//doesn't do any processing past the parser right now
-function comp(tree, opt) {
-  return tree;
-};
-
 //read file and send it over to comp
-function compfiles(paths, opt) {
+function compFiles(paths, opt) {
   if(typeof opt === 'function') opt = {cb:opt};
   Promise.map(paths, path => fs.readFileAsync(path, 'utf8').then(
-    data => parser.parse(data)
+    data => parser.parse(data) //parse each file
   )).then(parses => {
+    //concatenate the parse trees of our files
     let tree = [].concat.apply([], parses);
     opt.cb(comp(tree));
   });
@@ -29,7 +27,7 @@ function compfiles(paths, opt) {
 let api = {
   parser: parser,
   comp: comp,
-  compfiles: compfiles
+  compFiles: compFiles
 };
 
 module.exports = api;
